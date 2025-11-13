bfaf953087e32af0a868740bc0a826733e7884b1
Мирхайдарова Диана <lamyrr@bk.ru>
2025-11-14 00:50:20 +0500

    Проводим изменение QtWidgetsApplication.h - добавляем обработчики событий для реализации функциональности и контейнер для хранения кругов

--- Л3.1часть/QtWidgetsApplication.h
+++ Л3.1часть/QtWidgetsApplication.h
@@ -1,17 +1,24 @@
-#pragma once
+﻿#pragma once
 
 #include <QtWidgets/QMainWindow>
-#include "ui_QtWidgetsApplication.h"
+#include <QKeyEvent>
+#include "storage.h"  
 
 class QtWidgetsApplication : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    QtWidgetsApplication(QWidget *parent = nullptr);
+    QtWidgetsApplication(QWidget* parent = nullptr);
     ~QtWidgetsApplication();
 
-private:
-    Ui::QtWidgetsApplicationClass ui;
-};
+protected:
+   
+    void paintEvent(QPaintEvent* event) override;      // Для отрисовки кругов
+    void mousePressEvent(QMouseEvent* event) override; // Для кликов мыши
+    void keyPressEvent(QKeyEvent* event) override;     // Для клавиши Delete
+    void resizeEvent(QResizeEvent* event) override;    // Для изменения размера
 
+private:
+    Storage m_storage;  // наш контейнер для хранения кругов
+};
