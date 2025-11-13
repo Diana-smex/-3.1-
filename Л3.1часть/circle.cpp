b332d3e4e1481b50470aec13bfb31a01bb51ec5f
Мирхайдарова Диана <lamyrr@bk.ru>
2025-11-14 00:03:11 +0500

    создаём circle.cpp - здесь реализуем всю логику работы круга. Метод draw обеспечивает инкапсуляцию (круг сам себя рисует), contains проверяет клики, остальные управляют выделением

---
+++ Л3.1часть/circle.cpp
@@ -0,0 +1,32 @@
+﻿#include "circle.h"
+#include <QPainter>
+
+CCircle::CCircle(const QPoint& center, int radius)
+    : m_center(center), m_radius(radius), m_selected(false) {
+}
+
+void CCircle::draw(QPainter& painter) const {
+    if (m_selected) {
+        painter.setPen(QPen(Qt::red, 2));
+        painter.setBrush(QBrush(Qt::lightGray));
+    }
+    else {
+        painter.setPen(QPen(Qt::blue, 2));
+        painter.setBrush(QBrush(Qt::white));
+    }
+    painter.drawEllipse(m_center, m_radius, m_radius);
+}
+
+bool CCircle::contains(const QPoint& point) const {
+    int dx = point.x() - m_center.x();
+    int dy = point.y() - m_center.y();
+    return (dx * dx + dy * dy) <= (m_radius * m_radius);
+}
+
+void CCircle::setSelected(bool selected) {
+    m_selected = selected;
+}
+
+bool CCircle::isSelected() const {
+    return m_selected;
+}
