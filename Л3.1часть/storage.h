a5edf2a165a035ae3b2e2884c873e31fd16e6309
Мирхайдарова Диана <lamyrr@bk.ru>
2025-11-14 00:18:39 +0500

    Создание storage.h - это собственный контейнер, который хранит все созданные круги

---
+++ Л3.1часть/storage.h
@@ -0,0 +1,23 @@
+﻿#ifndef STORAGE_H
+#define STORAGE_H
+
+#include <QVector>
+#include "circle.h"
+
+class Storage {
+public:
+    Storage();
+
+    void addCircle(const CCircle& circle);
+    void removeCircle(int index);
+    CCircle& getCircle(int index);
+    int getCount() const;
+    void clear();
+
+    QVector<CCircle>& getCircles();
+
+private:
+    QVector<CCircle> m_circles;
+};
+
+#endif // STORAGE_H
