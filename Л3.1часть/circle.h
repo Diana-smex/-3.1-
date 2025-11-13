9aea87cb0a853f8e82b34e82733b72ec93cbe096
Мирхайдарова Диана <lamyrr@bk.ru>
2025-11-13 23:38:10 +0500

    Создаём файл circle.h - это будет наш графический объект "Круг"

---
+++ Л3.1часть/circle.h
@@ -0,0 +1,22 @@
+﻿#ifndef CIRCLE_H
+#define CIRCLE_H
+
+#include <QPoint>
+
+class CCircle {
+public:
+    CCircle(const QPoint& center, int radius = 20);
+
+    void draw(QPainter& painter) const;
+    bool contains(const QPoint& point) const;
+
+    void setSelected(bool selected);
+    bool isSelected() const;
+
+private:
+    QPoint m_center;
+    int m_radius;
+    bool m_selected;
+};
+
+#endif // CIRCLE_H
