a2377334f66fe77e1e4b29197b4892c91278c1ac
Мирхайдарова Диана <lamyrr@bk.ru>
2025-11-14 00:37:07 +0500

    Создание storage.cpp - реализация методов контейнера, которые позволяют добавлять, удалять и получать круги из контейнера.

---
+++ Л3.1часть/storage.cpp
@@ -0,0 +1,30 @@
+﻿#include "storage.h"
+
+Storage::Storage() {
+}
+
+void Storage::addCircle(const CCircle& circle) {
+    m_circles.append(circle);
+}
+
+void Storage::removeCircle(int index) {
+    if (index >= 0 && index < m_circles.size()) {
+        m_circles.removeAt(index);
+    }
+}
+
+CCircle& Storage::getCircle(int index) {
+    return m_circles[index];
+}
+
+int Storage::getCount() const {
+    return m_circles.size();
+}
+
+void Storage::clear() {
+    m_circles.clear();
+}
+
+QVector<CCircle>& Storage::getCircles() {
+    return m_circles;
+}
